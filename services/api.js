1107f56018882ea2d68d7d4738b63e51dfa08184
Vinícius Prates Araújo <vinicius.pratesaraujo@gmail.com>
2021-10-27 13:57:11 -0300

    att

--- services/api.js
+++ services/api.js
@@ -1,7 +1,18 @@
-import axios from 'axios'
+//<ROOT>/shared/APIKit.js
+import axios from 'axios';
 
-const api = axios.create({
-    baseUrl: 'http://localhost:5000'
-})
+// Create axios client, pre-configured with baseURL
+let Axios = axios.create({
+  baseURL: 'http://200.137.215.76:8080',
+  timeout: 10000,
+});
 
-export default api
+// Set JSON Web Token in Client to be included in all calls
+export const setClientToken = token => {
+  Axios.interceptors.request.use(function(config) {
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  });
+};
+
+export default Axios;
