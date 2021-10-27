b4adb872ae6f2bb3684a611b6c70040d0d1c51c4
Vinícius Prates Araújo <vinicius.pratesaraujo@gmail.com>
2021-10-27 13:56:33 -0300

    Commenting validation schema form for tests

--- constants/ValidationSchema/loginValidationSchema.tsx
+++ constants/ValidationSchema/loginValidationSchema.tsx
@@ -3,11 +3,11 @@ import * as yup from 'yup'
 const loginValidationSchema = yup.object().shape({
     cpf: yup
       .string()
-      .matches(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)
+      //.matches(/^\d{3}\\d{3}\\d{3}\\d{2}$/)
       .required('Insira seu cpf'),
     senha: yup
       .string()
-      .min(8, ({ min }) => `Senha deve possuir no minimo ${min} caracteres`)
+      //.min(8, ({ min }) => `Senha deve possuir no minimo ${min} caracteres`)
       .required('Insira sua senha!'),
   })
 export default loginValidationSchema
